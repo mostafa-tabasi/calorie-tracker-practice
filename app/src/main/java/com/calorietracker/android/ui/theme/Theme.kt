a464ef33609e1da0ca16675ebb81874df1382a0a
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2022-05-16 13:06:20 +0430

    Add ActionButton component in onboarding presentation module

--- app/src/main/java/com/calorietracker/android/ui/theme/Theme.kt
+++ app/src/main/java/com/calorietracker/android/ui/theme/Theme.kt
@@ -5,7 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import com.calorietracker.core_ui.Dimensions
+import com.calorietracker.core_ui.LocalSpacing
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
@@ -32,16 +35,22 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CalorieTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CalorieTrackerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    // It should be handle for different sizes of devices
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
